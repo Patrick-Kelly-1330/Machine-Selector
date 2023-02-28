8b1cdf69268f157484306fde9ac029a1b7fa5fb5
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-02-28 11:49:43 -0800

    implement dominos and falling back on first level

--- client/src/components/Machine.jsx
+++ client/src/components/Machine.jsx
@@ -6,7 +6,32 @@ const Machine = ({ names }) => {
   return (
     <div>
       <h1>Machine</h1>
-      <GroupMembers names={names} />
+      <div className="machineContainer">
+        <div className="dominosAndBall">
+          <div className="dominoBallContainer">
+            <div className="dominoBall"/>
+          </div>
+          <div className="dominosContainer">
+            <div className='dominoA'></div>
+            <div className='dominoB'></div>
+            <div className='dominoC'></div>
+            <div className='dominoD'></div>
+            <div className='dominoE'></div>
+            <div className='dominoF'></div>
+            <div className='dominoG'></div>
+            <div className='dominoH'></div>
+            <div className='dominoI'></div>
+            <div className='dominoJ'></div>
+          </div>
+        </div>
+        <div className="boxWithPlatform">
+        </div>
+        <div className="rampWithBall">
+        </div>
+        <div className="pegs">
+        </div>
+        <GroupMembers names={names} />
+      </div>
     </div>
   );
 }
